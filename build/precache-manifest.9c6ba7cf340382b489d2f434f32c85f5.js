bf45de9f54c431b19cdd00c442c3708db6d1da54
HeidiJoensuu <cedi@hotmail.fi>
2020-06-04 15:07:57 +0300

    mongodb exercise

---
+++ build/precache-manifest.9c6ba7cf340382b489d2f434f32c85f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0454b659af18658bd1d34c9df86c29e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c5854de1110e9676b92",
+    "url": "/static/css/main.085e14e2.chunk.css"
+  },
+  {
+    "revision": "107f4a056bf254f2692f",
+    "url": "/static/js/2.de702731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de702731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5854de1110e9676b92",
+    "url": "/static/js/main.29c5436f.chunk.js"
+  },
+  {
+    "revision": "dd35faaecda4ff65159e",
+    "url": "/static/js/runtime-main.8ff57c4b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -11,9 +14,11 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 
+/*
 let notes = [
   {
     id: 1,
@@ -34,56 +39,45 @@ let notes = [
     important: true
   }
 ]
-
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+*/
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
+  Note.filter
+
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-  const note = {
+
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
-  notes = notes.concat(note)
-  response.json(note)
+    important: body.important || false,
+    
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const PORT = process.env.PORT || 3001
